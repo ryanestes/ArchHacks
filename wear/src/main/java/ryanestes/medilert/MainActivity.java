397d3fe8b7e00a9a00ea5d6fea6f3d8fcc095ecf
Ryan Estes <restes93@gmail.com>
2016-11-05 23:55:09 -0500

    Added watch voice capabilities.

--- wear/src/main/java/ryanestes/medilert/MainActivity.java
+++ wear/src/main/java/ryanestes/medilert/MainActivity.java
@@ -1,12 +1,17 @@
 package ryanestes.medilert;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends Activity {
 
+    private static final int SPEECH_RECOGNIZER_REQUEST_CODE = 3000;
     private TextView mTextView;
 
     @Override
@@ -18,7 +23,38 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                displaySpeechRecognizer();
             }
         });
+    }
+
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent
+                (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Imitation Game\nScientist");
+        startActivityForResult(intent,
+                SPEECH_RECOGNIZER_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        if (requestCode == SPEECH_RECOGNIZER_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                List<String> results =
+                        data.getStringArrayListExtra
+                                (RecognizerIntent.EXTRA_RESULTS);
+                String spokenText = results.get(0);
+                startActivity();
+                if (spokenText.toUpperCase().indexOf("ALAN TURING") > -1)
+                    mTextView.setText("Correct!\nIt is Alan Turing.");
+                else
+                    mTextView.setText("Incorrect!\nIt is Alan Turing.");
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
